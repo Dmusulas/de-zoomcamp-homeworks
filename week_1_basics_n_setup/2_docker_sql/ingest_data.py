c2f578ab07515235dfdbdc1b838c640dbd0ba7f0
Dovydas <dovydas.musulas@gmail.com>
2023-02-02 22:40:11 +0200

    testing out prefect

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -6,59 +6,72 @@ from sqlalchemy import create_engine
 from time import time
 import argparse 
 import os
+from prefect import flow, task
+from prefect.tasks import task_input_hash
+from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
 
-def main(params):
-    user = params.user
-    password = params.password
-    host = params.host
-    port = params.port
-    db = params.db
-    table_name = params.table_name
-    url = params.url
+@task(log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def extract_data(url):
+    if url.endswith(".csv.gz"):
+        csv_name = "yellow_tripdata_2021-01.csv.gz"
+    else:
+        csv_name = "output.csv"
+    os.system(f"wget {url} -O {csv_name}")
 
-    csv_name = "output.csv"
-    # download_the_scv
-
-    os.system(f"wget -qO - {url} | gzip -d > {csv_name}")
-
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
-    engine.connect()
     df_iter = pd.read_csv(csv_name, iterator = True, chunksize = 100000)
-    df =pd.read_csv(csv_name, nrows=10)
+    df = next(df_iter)
 
-    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
-    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+    return df
 
+@task(log_prints=True)
+def transform_data(df):
+    print(f"Pre:missing passanger count: {df['passenger_count'].isin([0]).sum()}")
+    df = df[df['passenger_count'] != 0]
+    print(f"Post:missing passanger count: {df['passenger_count'].isin([0]).sum()}")
+    return df
 
-    while True:
-        t_start = time()
-
-        df = next(df_iter)
-
-        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
-        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
-
+@task(log_prints=True, retries=3)
+def ingest_data(table_name, df):
+    
+    connection_block = SqlAlchemyConnector.load("postgress")
+    
+    with connection_block.get_connection(begin=False) as engine:
+        df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+        
         df.to_sql(name=table_name, con=engine, if_exists="append")
 
-        t_end = time()
-        print(f"inserted another chunk...took {(t_end - t_start):.3}")
-
-
-if __name__ == "__main__":
-
+#    while True:
+#        t_start = time()
+#
+#        df = next(df_iter)
+#
+#        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+#        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+#
+#        df.to_sql(name=table_name, con=engine, if_exists="append")
+#
+#        t_end = time()
+#        print(f"inserted another chunk...took {(t_end - t_start):.3}")
+#
+@flow(name="Ingest flow")
+def main():
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres.')
-
-    parser.add_argument('--user', help='user name for postgres')
-    parser.add_argument('--password', help='user password for postgres')
-    parser.add_argument('--host', help='host for postgres')
-    parser.add_argument('--port', help='port for postgres')
-    parser.add_argument('--db', help='database name for postgres')
     parser.add_argument('--table_name', help='name of the table where we will write results to')
     parser.add_argument('--url', help='url of the csv file')
 
-    args = parser.parse_args()
-    main(args)
+    params = parser.parse_args()
+
+    table_name = params.table_name
+    url = params.url
+
+    raw_data = extract_data(url)
+    data = transform_data(raw_data) 
+    ingest_data(table_name, data)
+
+if __name__ == "__main__":
+    main()
 
